e7db2f96cba7a9f8bb71e9ec0e6ea87c30a06e88
Michael Watzko <michael@watzko.de>
2019-03-01 18:20:02 +0100

    Implement Error and Display for UperError

--- src/io/uper.rs
+++ src/io/uper.rs
@@ -18,6 +18,40 @@ pub enum Error {
     EndOfStream,
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::InvalidUtf8String => {
+                write!(f, "The underlying dataset is not a valid UTF8-String")
+            }
+            Error::UnsupportedOperation(o) => write!(f, "The operation is not supported: {}", o),
+            Error::InsufficientSpaceInDestinationBuffer => write!(
+                f,
+                "There is insufficient space in the destination buffer for this operation"
+            ),
+            Error::InsufficientDataInSourceBuffer => write!(
+                f,
+                "There is insufficient data in the source buffer for this operation"
+            ),
+            Error::ValueNotInRange(value, min, max) => write!(
+                f,
+                "The value {} is not within the inclusive range of {} and {}",
+                value, min, max
+            ),
+            Error::EndOfStream => write!(
+                f,
+                "Can no longer read or write any bytes from the underlying dataset"
+            ),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str {
+        "encoding or decoding UPER failed"
+    }
+}
+
 pub trait Uper {
     fn read_uper(reader: &mut Reader) -> Result<Self, Error>
     where
