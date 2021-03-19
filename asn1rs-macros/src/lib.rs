f8bcf1a7365bba24644eb37112e124722acfd8c6
Michael Watzko <michael@watzko.de>
2021-03-19 13:12:41 +0100

    Add runtime-macros hack to include proc macros in coverage report

--- asn1rs-macros/src/lib.rs
+++ asn1rs-macros/src/lib.rs
@@ -16,6 +16,10 @@ mod derive_protobuf_eq;
 #[proc_macro]
 pub fn asn_to_rust(item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as LitStr).value();
+    asn_to_rust_fn(&input).parse().unwrap()
+}
+
+fn asn_to_rust_fn(input: &str) -> String {
     let tokens = Tokenizer::default().parse(&input);
     let model = Model::try_from(tokens)
         .expect("Failed to parse tokens")
@@ -39,7 +43,7 @@ pub fn asn_to_rust(item: TokenStream) -> TokenStream {
         println!("-------- output end");
     }
 
-    output.parse().unwrap()
+    output
 }
 
 #[proc_macro_attribute]
@@ -58,4 +62,51 @@ pub fn protobuf_eq(input: TokenStream) -> TokenStream {
     }
 
     output
+}
+
+#[cfg(test)]
+mod proc_macro_tarpaulin_coverage_hack {
+    use super::asn_to_rust_fn;
+    use old_proc_macro2;
+    use old_syn;
+    use std::path::PathBuf;
+    use std::str::FromStr;
+    use std::{env, fs};
+
+    fn asn_to_rust_fn2(input: old_proc_macro2::TokenStream) -> old_proc_macro2::TokenStream {
+        let input = old_syn::parse2::<old_syn::LitStr>(input.into()).unwrap();
+        let result = asn_to_rust_fn(&input.value());
+        old_proc_macro2::TokenStream::from_str(&result).unwrap()
+    }
+
+    #[test]
+    fn cover_asn_to_rust_macro() {
+        // This code doesn't check much. Instead, it does macro expansion at run time to let
+        // tarpaulin measure code coverage for the macro.
+        let mut path = env::current_dir().unwrap();
+        path.pop();
+        path.push("tests");
+        walk_dir(path);
+    }
+
+    fn walk_dir(path: PathBuf) {
+        for entry in fs::read_dir(path).unwrap() {
+            match entry {
+                Ok(entry) if entry.file_type().unwrap().is_dir() => {
+                    walk_dir(entry.path());
+                }
+                Ok(entry) if entry.file_type().unwrap().is_file() => {
+                    println!("{:?}", entry.path());
+                    let file = fs::File::open(entry.path()).unwrap();
+                    runtime_macros::emulate_macro_expansion_fallible(
+                        file,
+                        "asn_to_rust",
+                        asn_to_rust_fn2,
+                    )
+                    .unwrap();
+                }
+                _ => {}
+            }
+        }
+    }
 }
