fcb198060477442998d4fca091b2ec0e262013b1
Michael Watzko <michael@watzko.de>
2021-03-19 16:43:36 +0100

    Reparse the result of asn_to_rust! to also track the expansion logic

--- tests/proc_macro_coverage_hack.rs
+++ tests/proc_macro_coverage_hack.rs
@@ -1,9 +1,14 @@
 use asn1rs::ast::asn_to_rust;
-use old_proc_macro2;
-use old_syn;
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+use std::io::Read;
+use std::panic::AssertUnwindSafe;
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::{env, fs};
+use std::{env, fs, panic};
+use syn::Attribute;
+use syn::ItemEnum;
+use syn::ItemStruct;
 
 #[test]
 fn cover_asn_to_rust_macro() {
@@ -23,22 +28,122 @@ fn walk_dir(path: PathBuf) {
                 walk_dir(entry.path());
             }
             Ok(entry) if entry.file_type().unwrap().is_file() => {
-                println!("{:?}", entry.path());
+                println!("Feeding {:?}", entry.path());
                 let file = fs::File::open(entry.path()).unwrap();
-                runtime_macros::emulate_macro_expansion_fallible(
-                    file,
-                    "asn_to_rust",
-                    asn_to_rust_fn2,
-                )
-                .unwrap();
+                emulate_macro_expansion_fallible(file);
             }
             _ => {}
         }
     }
 }
 
-fn asn_to_rust_fn2(input: old_proc_macro2::TokenStream) -> old_proc_macro2::TokenStream {
-    let input = old_syn::parse2::<old_syn::LitStr>(input.into()).unwrap();
-    let result = asn_to_rust(&input.value());
-    old_proc_macro2::TokenStream::from_str(&result).unwrap()
+/// Based on https://github.com/jeremydavis519/runtime-macros/blob/master/src/lib.rs
+///
+///
+/// This awfully great hack allows tarpaulin to track the proc-macro related function
+/// calls for a better line coverage. It manually parses each test file, takes the literal
+/// String of the 'asn_to_rust!()' macro and generates the models and then rust code for it.
+/// It then takes this intermediate result and feeds it to the proc-attribute expander to
+/// also track it.
+///
+/// WARNING: This does *NO* logic check and does *NOT* unit tests. It just helps to track the
+///          called functions and executed lines.
+///
+pub fn emulate_macro_expansion_fallible(mut file: fs::File) {
+    fn ast_parse_str(attr: &str, item: &str) -> TokenStream {
+        asn1rs::ast::parse(
+            TokenStream::from_str(attr).unwrap(),
+            TokenStream::from_str(item).unwrap(),
+        )
+    }
+
+    fn asn_to_rust_fn2(input: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+        let input = syn::parse2::<syn::LitStr>(input).unwrap();
+        let result = asn_to_rust(&input.value());
+        proc_macro2::TokenStream::from_str(&result).unwrap()
+    }
+
+    fn feed_derive_parser(
+        attributes: &[Attribute],
+        attribute_path: &syn::Path,
+        item: impl Fn() -> String,
+        body_start_marker: &str,
+    ) {
+        for attr in attributes {
+            if attr.path == *attribute_path {
+                let attribute_meta = attr.parse_meta().unwrap();
+                let attribute_meta = attribute_meta.into_token_stream().to_string();
+
+                let item = item();
+                // skip 'asn (' and ')'
+                let start = attribute_meta.find('(').unwrap();
+                let end = attribute_meta.rfind(')').unwrap();
+                let header = &attribute_meta[start + 1..end];
+                let body = {
+                    let body_start = item.find(body_start_marker).unwrap();
+                    &item[body_start..]
+                };
+
+                if cfg!(feature = "debug-proc-macro") {
+                    println!("##########: {}", item);
+                    println!("      meta: {}", attribute_meta);
+                    println!("    header: {}", header);
+                    println!("      body: {}", body);
+                    println!();
+                }
+
+                let result = ast_parse_str(header, body).to_string();
+
+                if result.contains("compile_error") {
+                    panic!("{}", result);
+                } else {
+                    syn::parse_str::<proc_macro2::TokenStream>(&result).expect("Result is invalid");
+                }
+                break;
+            }
+        }
+    }
+
+    struct MacroVisitor {
+        macro_path: syn::Path,
+        attribute_path: syn::Path,
+    }
+    impl<'ast> syn::visit::Visit<'ast> for MacroVisitor {
+        fn visit_item_enum(&mut self, i: &'ast ItemEnum) {
+            feed_derive_parser(
+                &i.attrs[..],
+                &self.attribute_path,
+                || i.into_token_stream().to_string(),
+                " pub enum ",
+            );
+        }
+
+        fn visit_item_struct(&mut self, i: &'ast ItemStruct) {
+            feed_derive_parser(
+                &i.attrs[..],
+                &self.attribute_path,
+                || i.into_token_stream().to_string(),
+                " pub struct ",
+            );
+        }
+
+        fn visit_macro(&mut self, macro_item: &'ast syn::Macro) {
+            if macro_item.path == self.macro_path {
+                let result = asn_to_rust_fn2(macro_item.tokens.clone());
+                let ast = AssertUnwindSafe(syn::parse_file(&result.to_string()).unwrap());
+                syn::visit::visit_file(self, &*ast);
+            }
+        }
+    }
+
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+
+    syn::visit::visit_file(
+        &mut MacroVisitor {
+            macro_path: syn::parse_str("asn_to_rust").unwrap(),
+            attribute_path: syn::parse_str("asn").unwrap(),
+        },
+        &syn::parse_file(content.as_str()).unwrap(),
+    )
 }
